11be6eb6b46d9d8ff71a7cc814a716fe765ee47a
OlegGeivandov <9327337@mail.ru>
2023-10-18 21:29:13 +0300

    123

--- src/main/java/com/example/binding/HelloController.java
+++ src/main/java/com/example/binding/HelloController.java
@@ -1,14 +1,43 @@
 package com.example.binding;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label outputLbl;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    TextField inputTxt;
+
+    @FXML
+    Button preBtn;
+    @FXML
+    Button postBtn;
+
+
+
+
+
+
+    Model model = new Model("Hello");
+
+    public void initialize(){
+        preBtn.setOnAction(actionEvent ->  model.preStar());
+        postBtn.setOnAction(actionEvent ->  model.postStar());
+
+
+
+
+
+        inputTxt.textProperty().bindBidirectional(model.s);
+        outputLbl.textProperty().bindBidirectional(model.s);
+
     }
+
+
+
+
 }
--- src/main/java/com/example/binding/Model.java
+++ src/main/java/com/example/binding/Model.java
@@ -1,2 +1,20 @@
-package com.example.binding;public class Model {
+package com.example.binding;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class Model {
+    SimpleStringProperty s = new SimpleStringProperty();
+
+    public Model(String str) {
+        s.set(str);
+    }
+
+    public void preStar(){
+        s.set("*" + s.get());
+    }
+
+    public void postStar(){
+        s.set(s.get() + "*");
+    }
+
 }
